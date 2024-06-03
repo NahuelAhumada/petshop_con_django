3b2f5a302118a2cecb363f47aa8770eafb8905ab
Nahuel <ahumada.nahuel35149@gmail.com>
2024-06-02 23:06:20 -0300

    Primer modelo de productos

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    producto = models.CharField(max_length=200)
+    precio= models.DecimalField(max_digits=12,decimal_places=2)
+    fecha_publicacion = models.DateTimeField('Fecha de publicación')
+    animal = models.CharField(max_length=20)
+    categoria = models.CharField(max_length=30)
+    marca = models.CharField(max_length=30)
