03dea470eeadbf073526393278be32ba9e4cd64e
Nahuel <ahumada.nahuel35149@gmail.com>
2024-07-04 17:35:14 -0300

    Update models

--- home/admin.py
+++ home/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from home.models import *
 
 
-admin.site.register(Categoria)
+admin.site.register(Animal)
+admin.site.register(Marca)
 admin.site.register(Producto)
 admin.site.register(Cliente)
 admin.site.register(Orden)
 admin.site.register(Orden_Producto)
-admin.site.register(Envio)
--- home/models.py
+++ home/models.py
@@ -24,21 +24,26 @@ class Orden(models.Model):
         orden_producto = self.orden_producto_set.all()
         total=sum([producto.cantidad for producto in orden_producto])
         return total
-class Categoria(models.Model):
+class Marca(models.Model):
+    nombre =models.CharField(max_length=100)
+    def __str__(self):
+        return self.nombre
+class Animal(models.Model):
     nombre = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=100, db_index=True)
 
     def __str__(self):
-        return 's%' % self.nombre
+        return self.nombre
 class Producto(models.Model):
     producto = models.CharField(max_length=200)
     precio= models.DecimalField(max_digits=12,decimal_places=2)
     fecha_publicacion = models.DateTimeField('Fecha de publicación')
     imagen = models.ImageField(upload_to="producto/%Y/%m/%d", blank=True, null=True)
-
+    animal = models.ForeignKey(Animal, blank=True, null=True, on_delete=models.CASCADE)
+    marca = models.ForeignKey(Marca, blank=True, null=True, on_delete=models.CASCADE)
     def __str__(self,):
         return self.producto + " --- " +str(self.fecha_publicacion)
-"""     @property
+""" @property
     def url_imagen(self):
         try:
             url = self.imagen.url
@@ -53,16 +58,5 @@ class Orden_Producto(models.Model):
     def total(self):
         return self.cantidad*self.producto.precio
 
-class Envio(models.Model):
-    cliente = models.ForeignKey(Cliente, on_delete=models.SET_NULL, blank=True, null=True)
-    orden = models.ForeignKey(Orden, on_delete=models.SET_NULL, blank=True, null=True)
-    fecha_agregado = models.DateTimeField(auto_now_add=True)
-    direccion = models.CharField(max_length=200)
-    localidad = models.CharField(max_length=200)
-    provincia = models.CharField(max_length=200)
-    codigo_postal = models.CharField(max_length=200)
-    def __str__(self):
-        return self.direccion
-class Marca(models.Model):
-    nombre =models.CharField(max_length=200)
+
     
