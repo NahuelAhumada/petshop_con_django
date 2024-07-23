8bc6b5e3ae79378d6810e586b36e799c38812eb3
NahuelAhumada <ahumada.nahuel35149@gmail.com>
2024-07-23 09:45:13 -0300

    foms.py de contacto

---
+++ contacto/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.forms import ModelForm
+from contacto.models import Consulta
+from captcha.fields import CaptchaField
+
+class ConsultaForm(ModelForm):
+    captcha = CaptchaField()
+    class Meta:
+        model = Consulta
+        fields = [
+            'nombre',
+            'descripcion',
+            'mail',
+        ]
+    def send_email(self, ):
+        nombre = self.cleaned_data['nombre']
+        descripcion = self.cleaned_data['descripcion']
+        mail = self.cleaned_data['mail']
+
+        #Logica de envio de mail
